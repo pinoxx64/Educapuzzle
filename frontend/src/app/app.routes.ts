aca57221c02b5ef4f94580bd23cd791910bdec8e
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-10-20 11:03:47 +0200

    Avance 20/10
    Creacion de la cabecera y preparación de las rutas de los botones de la cabecera

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './credenciales/login/login';
 import { RegistroComponent } from './credenciales/registro/registro';
+import { AuthGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
     { path: 'registro', component: RegistroComponent},
-    // { path: '',
-    //     //component: InicioComponent,
-    //     children: [
-
-    //     ]
-    // },
+    { path: '',
+        // component: conCabeceraComponent,
+        // children: [
+        //     { path: 'inicio', component: InicioComponent, canActivate: [AuthGuard] },
+        //     { path: 'gestionUser', component: GestionUserComponent, canActivate: [AuthGuard] },
+        //     { path: 'gestionPuzzle', component: GestionPuzzleComponent, canActivate: [AuthGuard] },
+        //     { path: 'verPuzzle', component: VerPuzzleComponent, canActivate: [AuthGuard] },
+        //     { path: 'resolverPuzzle', component: ResolverPuzzleComponent, canActivate: [AuthGuard] },
+        //     {path: 'ranking', component: RankingComponent, canActivate: [AuthGuard] }
+        // ]
+    },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: '**', redirectTo: '/login' }
 ];
--- frontend/src/app/shared/cabecera/cabecera.ts
+++ frontend/src/app/shared/cabecera/cabecera.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-cabecera',
-  imports: [],
   templateUrl: './cabecera.html',
-  styleUrl: './cabecera.css'
+  styleUrls: ['./cabecera.css'],
+  imports: [
+    CommonModule,
+    ButtonModule
+  ]
 })
-export class Cabecera {
+export class CabeceraComponent implements OnInit {
+  isAdmin: boolean = false;
+  isAlumno: boolean = false
+  isProfesor: boolean = false
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    const user = sessionStorage.getItem('user');
+    if (user) {
+      const usuario = JSON.parse(user);
+      this.isAdmin = usuario.user.roles.includes('Administrador');
+      this.isAlumno = usuario.user.roles.includes('Alumno')
+      this.isProfesor = usuario.user.roles.includes('Profesor');
+    }
+  }
+
+  inicio() {
+    this.router.navigate(['/inicio']);
+  }
+
+  gestionUser() {
+    this.router.navigate(['/gestionUser']);
+  }
+
+  gestionPuzzle() {    
+    this.router.navigate(['/gestionPuzzle']);
+  }
+
+  verPuzzle() {
+    this.router.navigate(['/verPuzzle']);
+  }
+
+  resolverPuzzle() {
+    this.router.navigate(['/resolverPuzzle']);
+  }
+
+  ranking() {
+    this.router.navigate(['/ranking']);
+  }
+
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
