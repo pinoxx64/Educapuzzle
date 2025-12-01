c1b46f9c4b5d61fa9812903c58cd25abe268773f
Raúl Gutiérrez Merino <loscanos3raul@gmail.com>
2025-12-01 14:30:57 +0100

    Avance 01/12
    Añadido el pie chart

--- frontend/src/app/con-cabecera/inicio/inicio.ts
+++ frontend/src/app/con-cabecera/inicio/inicio.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { EstadisticaService } from '../../service/estadistica.service';
+import { ChartModule } from 'primeng/chart';
 
 @Component({
   selector: 'app-inicio',
+  imports: [
+    ChartModule
+  ],
   templateUrl: './inicio.html',
   styleUrls: ['./inicio.css']
 })
+
 export class InicioComponent implements OnInit {
+
   usuario: any = null;
   estadisticas: any = null;
+
+  pieData: any;
+  pieOptions: any;
 
   constructor(private estadisticasService: EstadisticaService) {}
 
@@ -17,11 +26,34 @@ export class InicioComponent implements OnInit {
     this.usuario = usuarioActual ? JSON.parse(usuarioActual) : null;
     console.log('usuario', this.usuario);
 
-    if (this.usuario && this.usuario.user && this.usuario.user.id) {
+    if (this.usuario?.user?.id) {
       this.estadisticasService.getEstadisticas(this.usuario.user.id).subscribe(
         (response) => {
           this.estadisticas = response;
           console.log('estadisticas', response);
+
+          this.pieData = {
+            labels: ['Sudokus Ganados', 'Sudokus Jugados'],
+            datasets: [
+              {
+                data: [
+                  this.estadisticas.estadisticas.sudokuGanados,
+                  this.estadisticas.estadisticas.sudokuJugados
+                ],
+                backgroundColor: ['#42A5F5', '#66BB6A'],
+              }
+            ]
+          };
+          console.log('pieData', this.pieData);
+
+          this.pieOptions = {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top'
+              }
+            }
+          };
         },
         (error) => {
           console.error(error);
